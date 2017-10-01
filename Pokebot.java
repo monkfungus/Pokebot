5d3659b21dbdb1a0e6e5d266a08c09796c9edce3
monkfungus <s.nixon1@nuigalway.ie>
2017-10-01 23:01:43 +0100

    add reading from deets.txt capabilities

--- Pokebot.java
+++ Pokebot.java
@@ -1,28 +1,45 @@
 /* 
  * Basic skeleton of functionality. 
- *
+ * Reads details of the bot it controls and the room the bot
+ * lives in from deets.txt
+ * In deets.txt, format is as follows:
+ * <roomID> , <botID>
  */
 
 import java.util.Scanner;
+import java.io.*;
 
 public class Pokebot {
 
-	private static boolean getStatusSuccess;
-	private static boolean updateStatusSiccess;
+	private static String botID;
+	private static String roomID;
 
-	private static String userInput;
+	private static boolean getStatusSuccess;
 	private static String status;
+
 
 	public static void main(String args[]) {
 
-		userInput = new String();
+		
+		boolean updateStatusSiccess;
 		boolean validInput;
+
+		String userInput;
+		
+	
+		// read botID and roomID from deets.txt
+		try {
+			getIDs("deets.txt");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		Scanner scan = new Scanner(System.in);
 
+
 		System.out.println("=== Pokebot ===");
-
 		System.out.println();
-
+		System.out.println("Your room ID: " + roomID);
+		System.out.println("Your bot ID: " + botID);
 		System.out.println("Getting status ...");
 
 		getStatus();
@@ -35,7 +52,7 @@ public class Pokebot {
 		}
 		System.out.println();
 
-		// set validInput to false so while look will work
+		// set validInput to false so while loop will start
 		validInput = false;
 
 		while (validInput == false) {
@@ -71,6 +88,7 @@ public class Pokebot {
 	 */
 	private static void getStatus() {
 		
+		// assign getStatusSuccess as default
 		getStatusSuccess = true;
 
 		status = "poked";
@@ -85,4 +103,25 @@ public class Pokebot {
 		System.out.println("Poke sent.");
 	}
 
+
+	/*
+	 * getIDs reads from given file, parses text and updates roomID and botID
+	 * variables. File is assumed to be of structure "<roomID> , <botID>"
+	 */
+	private static void getIDs(String filename) throws Exception{
+
+		String line;
+		String arrIn[];
+
+		FileReader fr = new FileReader(filename);
+		BufferedReader br = new BufferedReader(fr);
+
+		line = br.readLine();
+
+		arrIn = line.split(" , ");
+		
+		// assign IDs from read in array
+		roomID = arrIn[0];
+		botID = arrIn[1];
+	}
 }
