6ffe85a632a01f3f2481cb0708d7ec9d4c19f7be
monkfungus <s.nixon1@nuigalway.ie>
2017-10-02 20:14:27 +0100

    update Pokebot.java to create a Spark.java instance
    * replace getStatusSuccess, status with isPoked
    * update getStatus() to return a boolean
    * update getIDs() to return a String[]
    * move while(validInput) loop into if(isPoked) loop for better readability

--- Pokebot.java
+++ Pokebot.java
@@ -10,88 +10,82 @@ import java.util.Scanner;
 import java.io.*;
 
 public class Pokebot {
-
-	private static String botID;
-	private static String roomID;
-
-	private static boolean getStatusSuccess;
-	private static String status;
-
-
+	
 	public static void main(String args[]) {
 
-		
-		boolean updateStatusSiccess;
 		boolean validInput;
-
 		String userInput;
-		
-	
+		String ids[];
+		boolean isPoked;
+
 		// read botID and roomID from deets.txt
 		try {
-			getIDs("deets.txt");
+			ids = getIDs("deets.txt");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+			ids = new String[2];
+			ids[0] = "deets.txt did not do a load";
+			ids[1] = "this bot is not the exist";
 		}
+
+		Spark spark = new Spark(ids);
+
 		Scanner scan = new Scanner(System.in);
 
 
 		System.out.println("=== Pokebot ===");
 		System.out.println();
-		System.out.println("Your room ID: " + roomID);
-		System.out.println("Your bot ID: " + botID);
+		System.out.println("Your room ID: " + ids[0]);
+		System.out.println("Your bot ID: " + ids[1]);
 		System.out.println("Getting status ...");
 
-		getStatus();
+		isPoked = getStatus();
 
-		if (getStatusSuccess == true) {
-			System.out.println("You are " + status);
+		if (isPoked == true) 
+		{
+			System.out.println("You are poked");
+			validInput = false; // force while to start
+
+			while (validInput == false) 
+			{
+				System.out.println("Poke back? (y/n)");
+				userInput = scan.nextLine();
+
+				if ( userInput.equals("y") == true ) 
+				{
+					System.out.println("sending poke ...");
+					sendPoke();
+					validInput = true;
+				}
+				else if ( userInput.equals("n") == true ) 
+				{
+					System.out.println("doing absolutely nothing about being poked");
+					validInput = true;
+				}
+				else {
+					System.out.println("press \"y\" or \"n\" or leave u goat");
+					validInput = false;
+				}
+			}
 		}
 		else  {
-			System.out.println("Failed to update status");
+			System.out.println("You are poking");
 		}
-		System.out.println();
-
-		// set validInput to false so while loop will start
-		validInput = false;
-
-		while (validInput == false) {
-
-			System.out.println("Poke back? (y/n)");
-			userInput = scan.nextLine();
-
-			if ( userInput.equals("y") == true) {
-				System.out.println("sending poke ...");
-				sendPoke();
-				validInput = true;
-
-			}
-			else if (userInput.equals("n") == true) {
-				System.out.println("doing absolutely nothing about being poked");
-				validInput = true;
-			}
-			else {
-				System.out.println("press \"y\" or \"n\" or leave u goat");
-				validInput = false;
-			}
-		}
-
+		System.out.println();		
 
 	}// end main
 
-	/*
-	 * getStatus checks and updates status
-	 * Modifies class variables getStatusSuccess and status
-	 * In future, will actually send a HTTP GET request to Cisco Spark
-	 * checking for @mentions in Spark room. Will update getStatus and
-	 * status accordingly. Will return a boolean and have exception handling.
-	 */
-	private static void getStatus() {
-		
-		// assign getStatusSuccess as default
-		getStatusSuccess = true;
 
-		status = "poked";
+	/*
+	 * getStatus gets and updates Status
+	 * Actually calls getLastMessage from spark and parses last message
+	 * to get the actual text of the last message this bot was tagged in.
+	 * Returns true if poked, false if not poked
+	 */
+	private static boolean getStatus() {
+
+		// temporary default
+		return true;
 	}
 
 
@@ -108,20 +102,18 @@ public class Pokebot {
 	 * getIDs reads from given file, parses text and updates roomID and botID
 	 * variables. File is assumed to be of structure "<roomID> , <botID>"
 	 */
-	private static void getIDs(String filename) throws Exception{
+	private static String[] getIDs(String filename) throws Exception{
 
-		String line;
-		String arrIn[];
+		String[] toReturn;
 
 		FileReader fr = new FileReader(filename);
 		BufferedReader br = new BufferedReader(fr);
 
-		line = br.readLine();
+		//line = br.readLine();
 
-		arrIn = line.split(" , ");
+		toReturn = br.readLine().split(" , ");
+
+		return toReturn;
 		
-		// assign IDs from read in array
-		roomID = arrIn[0];
-		botID = arrIn[1];
 	}
 }
